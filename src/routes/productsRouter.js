881fcefdf3cd57eb38d2b56997c64ba76fab1007
GuillermoCochrane <guilleac81@gmail.com>
2023-10-30 19:47:49 -0300

    Instalación y configuración de multer para productos, Modificación de categorías, Modificación de formularios de productos

--- src/routes/productsRouter.js
+++ src/routes/productsRouter.js
@@ -1,6 +1,23 @@
 const express = require('express');
 const router = express.Router();
+const path = require('path');
+const multer = require('multer');
 const productsController = require("../controllers/productsController")
+
+//Multer config
+
+const storage = multer.diskStorage({
+    destination: function(req, file, cb){
+        cb(null, path.join(__dirname, "../../public/images/products"))
+    },
+    filename: function(req,file, cb){
+        console.log(file);
+        let newFileName = "product" + Date.now() + path.extname(file.originalname)
+        cb(null, newFileName)
+    }
+})
+
+const upload = multer({storage})
 
 //Products Routes
 
@@ -20,11 +37,11 @@ router.get("/status/:idStatus",productsController.status)
 
 //Create Products
 router.get("/create",productsController.create)
-router.post("/create",productsController.store)
+router.post("/create",upload.single("img"),productsController.store)
 
 //Edit Products
 router.get('/edit/:id', productsController.edit); 
-router.put('/edit/:id', productsController.update); 
+router.put('/edit/:id', upload.single("img"), productsController.update); 
 
 //Product delete
 router.get('/delete/:id', productsController.delete);
