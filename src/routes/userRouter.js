44a4876350980302b59517745558d8ac16aff23f
GuillermoCochrane <guilleac81@gmail.com>
2023-11-01 17:08:29 -0300

    Finalizada configuración de multer para usuarios

--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -1,6 +1,23 @@
 const express = require('express');
 const router = express.Router();
+const path = require('path');
+const multer = require('multer');
 const userController = require('../controllers/userController');
+
+
+//Multer config
+
+const storage = multer.diskStorage({
+    destination: function(req, file, cb){
+        cb(null, path.join(__dirname, "../../public/images/users"))
+    },
+    filename: function(req,file, cb){
+        let newFileName = "user" + Date.now() + path.extname(file.originalname)
+        cb(null, newFileName)
+    }
+})
+
+const upload = multer({storage})
 
 //Users Routes
 
@@ -16,11 +33,11 @@ router.post('/login', userController.processLogin)
 
 //User Register
 router.get('/register', userController.register);
-router.post("/register", userController.store);
+router.post("/register",upload.single("photo"), userController.store);
 
 //Edit User
 router.get('/edit/:id', userController.edit); 
-router.put('/edit/:id', userController.update); 
+router.put('/edit/:id',upload.single("photo"), userController.update); 
 
 //Delete User
 router.get('/delete/:id', userController.delete);
