99675876d7d6e27ab07c2b7ce4db7a3bb8fd41fd
GuillermoCochrane <guilleac81@gmail.com>
2023-11-27 01:02:40 -0300

    Modificados estilos y animaciones de nav-bar

--- public/js/script.js
+++ public/js/script.js
@@ -2,15 +2,18 @@ window.addEventListener("load", function(){
     let burgermenu = this.document.querySelector(".burger-menu");
     let closemenu = this.document.querySelector(".close-menu");
     let nav = this.document.querySelector("#main-navbar");
+    let userLogged = this.document.querySelector(".userLogged-detail");
 
     burgermenu.addEventListener("click",function(){
         burgermenu.classList.toggle("hidden");
         closemenu.classList.toggle("hidden");
         nav.classList.toggle("hidden");
+        userLogged.classList.toggle("hidden");
     })
     closemenu.addEventListener("click",function(){
         burgermenu.classList.toggle("hidden");
         closemenu.classList.toggle("hidden");
         nav.classList.toggle("hidden");
+        userLogged.classList.toggle("hidden");
     })
 })
