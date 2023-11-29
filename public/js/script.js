f1dfcd303229f5c224d6396c9d52ac2e66bc1bcf
GuillermoCochrane <guilleac81@gmail.com>
2023-11-29 20:27:35 -0300

    Modificado header para todas las vistas

--- public/js/script.js
+++ public/js/script.js
@@ -1,23 +1,35 @@
 window.addEventListener("load", function(){
     let burgermenu = this.document.querySelector(".burger-menu");
+    let burgermenu1 = this.document.querySelector(".burger-menu1");
     let closemenu = this.document.querySelector(".close-menu");
+    let closemenu1 = this.document.querySelector(".close-menu1");
     let nav = this.document.querySelector("#main-navbar");
-    let userLogged = this.document.querySelector(".userLogged-detail");
-    let searchbar = this.document.querySelector(".search-bar1");
+    let userLogged = this.document.querySelector(".userLogged-detail-box");
+    let searchbar1 = this.document.querySelector(".search-bar1");
 
     burgermenu.addEventListener("click",function(){
         burgermenu.classList.toggle("hidden");
         closemenu.classList.toggle("hidden");
         nav.classList.toggle("hidden");
-        userLogged.classList.toggle("hidden");
-        searchbar.classList.toggle("hidden");
+        userLogged.classList.toggle("flex-hidden");
+        searchbar1.classList.toggle("hidden");
     })
     closemenu.addEventListener("click",function(){
         burgermenu.classList.toggle("hidden");
         closemenu.classList.toggle("hidden");
         nav.classList.toggle("hidden");
-        userLogged.classList.toggle("hidden");
-        searchbar.classList.toggle("hidden");
+        userLogged.classList.toggle("flex-hidden");
+        searchbar1.classList.toggle("hidden");
+    })
+    burgermenu1.addEventListener("click",function(){
+        burgermenu1.classList.toggle("hidden");
+        closemenu1.classList.toggle("hidden");
+        nav.classList.toggle("hidden");
+    })
+    closemenu1.addEventListener("click",function(){
+        burgermenu1.classList.toggle("hidden");
+        closemenu1.classList.toggle("hidden");
+        nav.classList.toggle("hidden");
     })
 
 })
