e88e6269a246b450936ec32be63bae1bfc1145d6
GuillermoCochrane <guilleac81@gmail.com>
2023-11-29 19:18:05 -0300

    Modificado header fixed para vista mobile

--- public/js/script.js
+++ public/js/script.js
@@ -3,17 +3,21 @@ window.addEventListener("load", function(){
     let closemenu = this.document.querySelector(".close-menu");
     let nav = this.document.querySelector("#main-navbar");
     let userLogged = this.document.querySelector(".userLogged-detail");
+    let searchbar = this.document.querySelector(".search-bar1");
 
     burgermenu.addEventListener("click",function(){
         burgermenu.classList.toggle("hidden");
         closemenu.classList.toggle("hidden");
         nav.classList.toggle("hidden");
         userLogged.classList.toggle("hidden");
+        searchbar.classList.toggle("hidden");
     })
     closemenu.addEventListener("click",function(){
         burgermenu.classList.toggle("hidden");
         closemenu.classList.toggle("hidden");
         nav.classList.toggle("hidden");
         userLogged.classList.toggle("hidden");
+        searchbar.classList.toggle("hidden");
     })
+
 })
