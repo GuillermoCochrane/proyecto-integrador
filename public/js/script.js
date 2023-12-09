d1c160e3bcabcef17717c1947ae1de7e2d0cb6d2
GuillermoCochrane <guilleac81@gmail.com>
2023-12-09 11:33:28 -0300

    Editada funcionalidad de botones en  profile

--- public/js/script.js
+++ public/js/script.js
@@ -1,4 +1,5 @@
 window.addEventListener("load", function(){
+    //header
     let burgermenu = this.document.querySelector(".burger-menu");
     let burgermenu1 = this.document.querySelector(".burger-menu1");
     let closemenu = this.document.querySelector(".close-menu");
@@ -13,23 +14,44 @@ window.addEventListener("load", function(){
         nav.classList.toggle("hidden");
         userLogged.classList.toggle("flex-hidden");
         searchbar1.classList.toggle("hidden");
-    })
+    });
     closemenu.addEventListener("click",function(){
         burgermenu.classList.toggle("hidden");
         closemenu.classList.toggle("hidden");
         nav.classList.toggle("hidden");
         userLogged.classList.toggle("flex-hidden");
         searchbar1.classList.toggle("hidden");
-    })
+    });
     burgermenu1.addEventListener("click",function(){
         burgermenu1.classList.toggle("hidden");
         closemenu1.classList.toggle("hidden");
         nav.classList.toggle("hidden");
-    })
+    });
     closemenu1.addEventListener("click",function(){
         burgermenu1.classList.toggle("hidden");
         closemenu1.classList.toggle("hidden");
         nav.classList.toggle("hidden");
-    })
+    });
+
+// profile 
+
+    let profileButton = this.document.querySelector("#profile-data-button");
+    let historyButton = this.document.querySelector("#profile-history-button");
+    let profileData = this.document.querySelector("#profile-user-data");
+    let purchaseHistory = this.document.querySelector("#purchase-history");
+
+    profileButton.addEventListener("click",function(){
+        profileButton.classList.toggle("hidden");
+        historyButton.classList.toggle("hidden");
+        profileData.classList.toggle("hidden");
+        purchaseHistory.classList.toggle("hidden");
+    });
+
+    historyButton.addEventListener("click",function(){
+        profileButton.classList.toggle("hidden");
+        historyButton.classList.toggle("hidden");
+        profileData.classList.toggle("hidden");
+        purchaseHistory.classList.toggle("hidden");
+    });
 
 })
