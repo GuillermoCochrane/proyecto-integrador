284a3564b104614bbeab69bdc74667447f85a96f
GuillermoCochrane <guilleac81@gmail.com>
2024-04-18 17:56:57 -0300

    Modificado script de botones del dashboard para elegir vistas

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -1,11 +1,19 @@
-function toggleElement(buttonNumber) {
-    var products = document.getElementById("dashboard-details");
-    var table = document.getElementById("dashboard-table");
-    if (buttonNumber === 1) {
-        products.style.display = "block";
-        table.style.display = "none"
-    } else if (buttonNumber === 2) {
-        products.style.display = "none";
-        table.style.display = "block"
+window.addEventListener("load", () =>{
+    let products = document.getElementById("dashboard-details");
+    let table = document.getElementById("dashboard-table");
+    let button1 = document.getElementById("button1");
+    let button2 = document.getElementById("button2");
+
+    button1.addEventListener("click", () => toggleElement(1));
+    button2.addEventListener("click", () => toggleElement(2));
+
+    function toggleElement(buttonNumber) {
+        if (buttonNumber === 1) {
+            products.style.display = "block";
+            table.style.display = "none"
+        } else if (buttonNumber === 2) {
+            products.style.display = "none";
+            table.style.display = "block"
+        }
     }
-}
+})
