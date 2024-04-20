a08cf7b60f9d9d47cc599ba72b5bd45e308b51a2
GuillermoCochrane <guilleac81@gmail.com>
2024-04-19 22:29:07 -0300

    Modificados estilo del contador  en  detalle de productos

--- public/js/productDetailCounter.js
+++ public/js/productDetailCounter.js
@@ -2,17 +2,21 @@ window.addEventListener("load", ()=>{
     let $increase = document.querySelector("#increase");
     let $decrease = document.querySelector("#decrease");
     let $counter = document.querySelector("#counter");
+    let $display = document.querySelector("#display")
     let counterValue = parseInt($counter.value);
+    $display.innerHTML = counterValue
 
     $increase.addEventListener("click", (e)=>{
         e.preventDefault()
         counterValue += 1;
-        $counter.value = counterValue
+        $counter.value = counterValue;
+        $display.innerHTML = counterValue;
     });
 
     $decrease.addEventListener("click", (e)=>{
         e.preventDefault();
         counterValue > 1 ? counterValue -= 1 : null;
-        $counter.value = counterValue
+        $counter.value = counterValue;
+        $display.innerHTML = counterValue;
     });
 })
