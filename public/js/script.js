1d835306f4830bcd051f584e0175e1e545102e93
GuillermoCochrane <guilleac81@gmail.com>
2024-04-21 03:35:49 -0300

    Modificado header

--- public/js/script.js
+++ public/js/script.js
@@ -1,36 +1,28 @@
 window.addEventListener("load", function(){
     //header
     let burgermenu = this.document.querySelector(".burger-menu");
-    let burgermenu1 = this.document.querySelector(".burger-menu1");
     let closemenu = this.document.querySelector(".close-menu");
-    let closemenu1 = this.document.querySelector(".close-menu1");
     let nav = this.document.querySelector("#main-navbar");
     let userLogged = this.document.querySelector(".userLogged-detail-box");
-    let searchbar1 = this.document.querySelector(".search-bar1");
+    let searchbar = this.document.querySelector("#search-bar");
 
     burgermenu.addEventListener("click",function(){
         burgermenu.classList.toggle("hidden");
         closemenu.classList.toggle("hidden");
         nav.classList.toggle("hidden");
-        userLogged.classList.toggle("flex-hidden");
-        searchbar1.classList.toggle("hidden");
+        if (window.innerWidth < 768) {
+            userLogged.classList.toggle("flex-hidden");
+            searchbar.classList.toggle("flex-hidden");
+        }
+        
     });
     closemenu.addEventListener("click",function(){
         burgermenu.classList.toggle("hidden");
         closemenu.classList.toggle("hidden");
         nav.classList.toggle("hidden");
-        userLogged.classList.toggle("flex-hidden");
-        searchbar1.classList.toggle("hidden");
+        if (window.innerWidth < 768) {
+            userLogged.classList.toggle("flex-hidden");
+            searchbar.classList.toggle("flex-hidden");
+        }
     });
-    burgermenu1.addEventListener("click",function(){
-        burgermenu1.classList.toggle("hidden");
-        closemenu1.classList.toggle("hidden");
-        nav.classList.toggle("hidden");
-    });
-    closemenu1.addEventListener("click",function(){
-        burgermenu1.classList.toggle("hidden");
-        closemenu1.classList.toggle("hidden");
-        nav.classList.toggle("hidden");
-    });
-
 })
