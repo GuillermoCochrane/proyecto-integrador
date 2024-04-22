a276eb8fd91f37dcfc345d6670900c2a98c9c89e
GuillermoCochrane <guilleac81@gmail.com>
2024-04-22 00:33:17 -0300

    Agregada lógica de búsqueda predictiva

--- public/js/script.js
+++ public/js/script.js
@@ -1,11 +1,13 @@
 window.addEventListener("load", function(){
-    //header
     let burgermenu = this.document.querySelector(".burger-menu");
     let closemenu = this.document.querySelector(".close-menu");
     let nav = this.document.querySelector("#main-navbar");
     let userLogged = this.document.querySelector(".userLogged-detail-box");
     let searchbar = this.document.querySelector("#search-bar");
+    let $searchInput = this.document.querySelector("#search-input");
+    let $list = this.document.querySelector("#result-list");
 
+    // hidden navbar
     burgermenu.addEventListener("click",function(){
         burgermenu.classList.toggle("hidden");
         closemenu.classList.toggle("hidden");
@@ -26,4 +28,29 @@ window.addEventListener("load", function(){
             userLogged ? userLogged.classList.toggle("flex-hidden") : null ;
         }
     });
+
+    //predictive searchbar
+    $searchInput.addEventListener("input", () => {
+        $list.innerHTML = null;
+        if ($searchInput.value.lenght != 0) {
+            $list.classList.remove("hidden");
+            fetch(`http://localhost:3003/api/search?search=${$searchInput.value}`)
+            .then(res => res.json())
+            .then(info =>{
+                if(info.data.lenght != 0){
+                    for (const product of info.data) {
+                        $list.innerHTML += `<li><a href="/products/${product.id}"> ${product.name} </a></li>`
+                    }
+                } else {
+                    $list.innerHTML += "<li>No se encontraron productos...</li>"
+                }
+            })
+        } else {
+            $list.innerHTML = null;
+        }
+    })
+
+   /*  $searchInput.addEventListener("blur",()=>{
+        $list.classList.add("hidden");
+    }) */
 })
