aea546bbcbb83bc57af190338ddff78433d5befa
GuillermoCochrane <guilleac81@gmail.com>
2024-04-23 18:16:16 -0300

    finalizadas validaciones email

--- public/js/registerValidations.js
+++ public/js/registerValidations.js
@@ -1,6 +1,7 @@
 window.addEventListener("load", ()=>{
     const $username = document.querySelector("#username");
     const $email = document.querySelector("#email");
+    const $btn = document.querySelector("#register-btn")
 
     let errors = { };
 
@@ -19,10 +20,11 @@ window.addEventListener("load", ()=>{
 
     const lengthValidation = (input,min,max) => {
         let error = document.querySelector( `#error-${input.id}`);
+        let label = input.id;
         if(!validator.isLength(input.value, {min,max})){
             let errormsg = `El ${input.id} debe tener entre ${min} y ${max} caracteres`;
             error.innerText = errormsg;
-            errors.input = errormsg;
+            errors[label] = errormsg;
         }else{
             error.innerText = '';
             delete errors.input;
@@ -32,12 +34,36 @@ window.addEventListener("load", ()=>{
     const usernameValidation = () => {
         return () => {
             requiredValidation($username);
-            lengthValidation($username, 3,5);
+            lengthValidation($username, 3,10);
         }
     };
 
+    let emailValidation = () => {
+        let error = document.querySelector(`#error-${$email.id}`);
+        return () => {
+
+            requiredValidation($email);
+            lengthValidation($email,8,40);
+
+            if(!validator.isEmail($email.value)){
+                error.innerText = 'El email no es válido' ;
+                errors.email = 'El email no es válido';
+            }else{
+                error.innerText = '';
+                delete errors.email;
+            }
+
+        }
+    }
+
     $username.addEventListener('input', usernameValidation());
     $username.addEventListener('blur', usernameValidation());
+    $email.addEventListener("input", emailValidation());
+    $email.addEventListener("blur",emailValidation());
 
-    console.log(errors);
+    $btn.addEventListener("click", (e)=>{
+        e.preventDefault();
+        console.log(errors);
+    })
+
 })
