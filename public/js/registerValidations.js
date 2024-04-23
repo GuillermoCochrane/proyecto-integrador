541594ab837589c343c63526886b52fcace8c1d2
GuillermoCochrane <guilleac81@gmail.com>
2024-04-23 19:58:34 -0300

    Agregadas validaciones de frontend en register para password

--- public/js/registerValidations.js
+++ public/js/registerValidations.js
@@ -1,6 +1,7 @@
 window.addEventListener("load", ()=>{
     const $username = document.querySelector("#username");
     const $email = document.querySelector("#email");
+    const $pass = document.querySelector("#password")
     const $btn = document.querySelector("#register-btn")
 
     let errors = { };
@@ -46,8 +47,9 @@ window.addEventListener("load", ()=>{
             lengthValidation($email,8,40);
 
             if(!validator.isEmail($email.value)){
-                error.innerText = 'El email no es válido' ;
-                errors.email = 'El email no es válido';
+                let errormsg = "El email no es válido";;
+                error.innerText = errormsg;
+                errors.email = errormsg;
             }else{
                 error.innerText = '';
                 delete errors.email;
@@ -56,10 +58,28 @@ window.addEventListener("load", ()=>{
         }
     }
 
+    let passwordValidation = () => {
+        let error = document.querySelector(`#error-${$pass.id}`);
+        return () => {
+            requiredValidation(password);
+            lengthValidation($pass,8,16);
+            if(!validator.isStrongPassword($pass.value)){
+                let errormsg = 'La contraseña debe tener al menos 8 caracteres, 1 mayúscula, 1 minúscula, 1 número y 1 símbolo';
+                error.innerText = errormsg;
+                errors.password = errormsg;
+            }else{
+                error.innerText = '';
+                delete errors.password;
+            }
+        }
+    }
+
     $username.addEventListener('input', usernameValidation());
     $username.addEventListener('blur', usernameValidation());
     $email.addEventListener("input", emailValidation());
     $email.addEventListener("blur",emailValidation());
+    $pass.addEventListener('input', passwordValidation());
+    $pass.addEventListener('blur', passwordValidation());
 
     $btn.addEventListener("click", (e)=>{
         e.preventDefault();
--- src/middlewares/userPasswordValidationsMDW.js
+++ src/middlewares/userPasswordValidationsMDW.js
@@ -4,11 +4,12 @@ const usertValidations = [
     body('password')
         .notEmpty().withMessage('Debes ingresar una contraseña').bail()
         .isLength({min:8, max:16}).withMessage("La contraseña debe tener entre 8 y 16 caracteres").bail()
+        .isStrongPassword().withMessage("La contraseña debe tener al menos 8 caracteres, 1 mayúscula, 1 minúscula, 1 número y 1 símbolo").bail()
         .custom((value, {req}) => {
         if(value != req.body.confirm){
             throw new Error("Las contraseñas no coinciden");
         }
         return true
-    }),//validar password (strong) 
+    }),
 ]
 module.exports = usertValidations
