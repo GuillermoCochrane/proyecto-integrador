bb06afbf917deb64cd97ef44201653f1f270937f
GuillermoCochrane <guilleac81@gmail.com>
2024-04-24 07:55:27 -0300

    endpoint para comprobar si el nombre de usuario esta en uso

--- src/controllers/API/usersAPIController.js
+++ src/controllers/API/usersAPIController.js
@@ -14,6 +14,20 @@ const usersAPIController = {
         }
         return res.json(info)
     },
+
+    usernameCheck: function(req,res){
+        let data = usersFunctions.filterByKey(req.params.username, "username")[0];
+        let response = true;
+        data ? response = true : response = false;
+        let info = {
+            meta: {
+                status : 200,
+                url: 'api/users/username/:username'
+            },
+            inUse: response,
+        }
+        return res.json(info)
+    },
 }
 
 module.exports = usersAPIController
--- src/routes/API/usersApiRouter.js
+++ src/routes/API/usersApiRouter.js
@@ -2,6 +2,6 @@ const express = require('express');
 const router = express.Router();
 const usersAPIController = require("../../controllers/API/usersAPIController");
 
-router.get("/email/:email", usersAPIController.emailCheck)
-
+router.get("/email/:email", usersAPIController.emailCheck);
+router.get("/username/:username", usersAPIController.usernameCheck);
 module.exports = router
