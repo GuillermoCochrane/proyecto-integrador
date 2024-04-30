e6c41e72c88d1a11444dc56588afb39b2e3bea11
GuillermoCochrane <guilleac81@gmail.com>
2024-04-30 17:11:30 -0300

    Finalizadas validaciones de front end de login

--- public/js/loginValidations.js
+++ public/js/loginValidations.js
@@ -4,7 +4,6 @@ window.addEventListener("load", ()=>{
     const $btn = document.querySelector("#login-btn");
     const $form = document.querySelector("#login-form");
 
-    console.log($username);
     let errors = { };
 
     const inputError = (input)=>{
@@ -19,9 +18,10 @@ window.addEventListener("load", ()=>{
 
     const requiredValidation = (input) => {
         let error = document.querySelector(`#error-${input.id}`);
+        console.log(error);
         let label = input.id;
         if(validator.isEmpty(input.value)){
-            let errormsg = "Debe ingresar el nombre de usuario";
+            let errormsg = "Debe ingresar el " + input.id;
             error.innerText =  errormsg;
             errors[label] = errormsg;
             inputError(input);
@@ -46,13 +46,20 @@ window.addEventListener("load", ()=>{
         };
     };
 
+    let passwordValidation = async () => {
+        requiredValidation($password);
+    };
+
     $username.addEventListener('input', () => usernameValidation());
     $username.addEventListener('blur', () => usernameValidation());
+    $password.addEventListener("input", ()=> passwordValidation());
+    $password.addEventListener("blur", ()=> passwordValidation());
 
     $btn.addEventListener("click", async(e)=>{
         e.preventDefault();
 
         usernameValidation();
+        passwordValidation();
 
         if (Object.keys(errors).length == 0) {
             $form.submit();
