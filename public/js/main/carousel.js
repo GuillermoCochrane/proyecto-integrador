4d594742d4370f9df74cd4173fb3f992fd77f25d
GuillermoCochrane <guilleac81@gmail.com>
2024-05-12 01:50:57 -0300

    script para funcionamiento de carrusel

---
+++ public/js/main/carousel.js
@@ -0,0 +1,16 @@
+const swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    breakpoints: {
+        768: {
+            slidesPerView: 2,
+        },
+        1200: {
+            slidesPerView: 4,
+        }
+    },
+    loop: true,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
