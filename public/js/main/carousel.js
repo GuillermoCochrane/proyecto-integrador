e9934b03ce46ab72f5536ea8baa9699283a3aa4c
GuillermoCochrane <guilleac81@gmail.com>
2024-05-12 02:40:24 -0300

    Agregado carrusel para la parte inferior de home

--- public/js/main/carousel.js
+++ public/js/main/carousel.js
@@ -14,3 +14,20 @@ const swiper = new Swiper(".mySwiper", {
         prevEl: ".swiper-button-prev",
     },
 });
+
+const carrouselBottom = new Swiper("#carrousel-bottom", {
+    slidesPerView: 1,
+    breakpoints: {
+        768: {
+            slidesPerView: 2,
+        },
+        1200: {
+            slidesPerView: 4,
+        }
+    },
+    loop: true,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
