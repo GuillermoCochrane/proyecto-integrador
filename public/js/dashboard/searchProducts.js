2257c1c161cc8061d7cf75deb06c98c4aac5bef5
GuillermoCochrane <guilleac81@gmail.com>
2024-05-16 17:25:02 -0300

    Creado script para barra de búsqueda predictiva para productos en el dashboard

---
+++ public/js/dashboard/searchProducts.js
@@ -0,0 +1,44 @@
+window.addEventListener("load", function(){
+    let searchbar = this.document.querySelector("#search-bar");
+    let $searchInput = this.document.querySelector("#search-input");
+    let $list = this.document.querySelector("#result-list");
+
+    //predictive searchbar
+    $searchInput.addEventListener("input", () => {
+        $list.innerHTML = null;
+        if ($searchInput.value.lenght != 0) {
+            $list.classList.remove("hidden");
+            fetch(`http://localhost:3003/api/search?search=${$searchInput.value}`)
+            //http://localhost:3003 dev https://multihogar.onrender.com production
+            .then(res => res.json())
+            .then(info =>{
+                if(info.data.lenght != 0){
+                    for (const product of info.data) {
+                        $list.innerHTML += `<li><a href="/dashboard/products/${product.id}"> ${product.name} </a></li>`
+                    }
+                } else {
+                    $list.innerHTML += "<li>No se encontraron productos...</li>"
+                }
+            })
+        } else {
+            $list.innerHTML = null;
+        }
+    });
+
+    searchbar.addEventListener("mouseleave", () => {
+        $list.classList.add("hidden");
+    });
+
+    searchbar.addEventListener("touchstart", (e) => {
+        if(!searchbar.contains(e.target)){
+            $list.classList.add("hidden");
+        }
+    })
+})
+
+//Ignore changes on this file for new commits
+//git update-index --assume-unchanged public/js/script.js
+//not ignore changes on this file for new
+//git update-index --no-assume-unchanged public/js/script.js
+//list o ignored files for new commits
+//git ls-files -v | grep "^[[:lower:]]"
